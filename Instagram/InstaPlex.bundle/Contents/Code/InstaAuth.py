c87eb06245dda079bd90c1fbfe6f6ad5c59d3542
Oliver Knoll <starsky.hutch@gmx.ch>
2011-12-04 17:05:13 +0100

    Simplyfied WebKeys

--- Instagram/InstaPlex.bundle/Contents/Code/InstaAuth.py
+++ Instagram/InstaPlex.bundle/Contents/Code/InstaAuth.py
@@ -1,7 +1,9 @@
 '''
+Implements authorisation and authentication using OAuth 2.0 protocol with Instagram.
+
 Created on 29.11.2011
 
-@author: tknoll
+@author: Oliver Knoll
 '''
 
 import urllib2
@@ -9,27 +11,17 @@ import urllib
 import cookielib
 import WebKeys
 
-COOKIE_FILE = "InstaCookie.txt"
+COOKIE_FILE = 'InstaCookie.txt'
 
 class InstaAuth:
     
-    clientId = None
-    redirectUri = None
     myRedirectHandler = None
      
-    def __init__(self, clientId, redirectUri):
-        self.clientId = clientId
-        self.redirectUri = redirectUri
+    def __init__(self):
         self.myRedirectHandler = MyHTTPRedirectHandler()
         cj = cookielib.LWPCookieJar(COOKIE_FILE)        
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), self.myRedirectHandler)
         urllib2.install_opener(opener)
-        
-    def getClientId(self):
-        return self.clienId
-    
-    def getRedirectUri(self):
-        return self.redirectUri
     
     def authorize(self):
         
@@ -37,33 +29,32 @@ class InstaAuth:
      
         try:
             # Login
-            Log.Debug('Login in: ' + WebKeys.LOGIN_URL % (self.clientId, self.redirectUri))
-            param = urllib.urlencode({"password" : Prefs['password'], "username": Prefs['username']})
-            req = urllib2.Request(WebKeys.LOGIN_URL % (self.clientId, self.redirectUri), data = param)
+            Log.Debug('Login in...')
+            param = urllib.urlencode({'password' : Prefs['password'], 'username': Prefs['username']})
+            req = urllib2.Request(WebKeys.LOGIN_URL, data = param)
             response = urllib2.urlopen(req)
         except urllib2.HTTPError:
-            Log.Debug("HTTP error happened")
+            Log.Debug('HTTP error happened')
         
         try:
             # Authorize
-            Log.Debug('Authorise: ' + WebKeys.HTTPS_AUTH_URL % (self.clientId, self.redirectUri))
-            req = urllib2.Request(WebKeys.HTTPS_AUTH_URL % (self.clientId, self.redirectUri))
+            Log.Debug('Authorise...')
+            req = urllib2.Request(WebKeys.HTTPS_AUTH_URL)
             response = urllib2.urlopen(req)
         except urllib2.HTTPError:
-            Log.Debug("HTTP error happened")
+            Log.Debug('HTTP error happened')
             
         token = self.myRedirectHandler.getToken()    
         if token == None:
             try:
                 # Allow application
-                param = urllib.urlencode({"allow" : "Yes"})
-                Log.Debug('Allow: ' + WebKeys.HTTP_AUTH_URL % (self.clientId, self.redirectUri))
-                req = urllib2.Request(WebKeys.HTTP_AUTH_URL % (self.clientId, self.redirectUri), data=param)
+                param = urllib.urlencode({'allow' : 'Yes'})
+                req = urllib2.Request(WebKeys.HTTP_AUTH_URL, data=param)
                 response = urllib2.urlopen(req)
             except urllib2.HTTPError:
-                Log.Debug("HTTP error happened")
+                Log.Debug('HTTP error happened')
         
-        Log.Debug("Returning the Token:")
+        Log.Debug('Returning the Token:')
         Log.Debug(self.myRedirectHandler.getToken())
         return self.myRedirectHandler.getToken()
         
@@ -78,9 +69,8 @@ class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
             newurl = headers.getheaders('uri')[0]
         
         if newurl.startswith(WebKeys.REDIRECT_URI):
-            tokens = newurl.split("=")
+            tokens = newurl.split('=')
             self.token = tokens[1]
-            Log.Debug("Token: " + tokens[1])
         else:
             self.token = None
         
--- Instagram/InstaPlex.bundle/Contents/Code/WebKeys.py
+++ Instagram/InstaPlex.bundle/Contents/Code/WebKeys.py
@@ -1,21 +1,20 @@
 '''
 Created on 29.11.2011
 
-@author: tknoll
+@author: Oliver Knoll
 '''
 
 CLIENT_ID    = 'c5142704e6104afe89552d7d170e6915'
 REDIRECT_URI = 'http://www.till-art.net/instaplex'
 
-'''First arg (str): client ID Second arg (str): Redirect-URI'''
-HTTPS_AUTH_URL = 'https://instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=token'
-HTTP_AUTH_URL  = 'http://instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=token'
-LOGIN_URL      = 'https://instagram.com/accounts/login/?next=/oauth/authorize/&client_id=%s&redirect_uri=%s&response_type=token'
+HTTPS_AUTH_URL = 'https://instagram.com/oauth/authorize/?client_id=' + CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=token'
+HTTP_AUTH_URL  = 'http://instagram.com/oauth/authorize/?client_id=' + CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=token'
+LOGIN_URL      = 'https://instagram.com/accounts/login/?next=/oauth/authorize/&client_id=' + CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=token'
 
 # End points
 
-POPULAR_URL = 'https://api.instagram.com/v1/media/popular?client_id='
+POPULAR_URL = 'https://api.instagram.com/v1/media/popular?client_id=' + CLIENT_ID
 MY_PHOTOS_URL = 'https://api.instagram.com/v1/users/self/media/recent?access_token='
-ZURICH_URL = 'https://api.instagram.com/v1/tags/zurich/media/recent?client_id='
-NEWYORK_URL = 'https://api.instagram.com/v1/tags/newyork/media/recent?client_id='
-PARIS_URL = 'https://api.instagram.com/v1/tags/paris/media/recent?client_id='
+ZURICH_URL = 'https://api.instagram.com/v1/tags/zurich/media/recent?client_id=' + CLIENT_ID
+NEWYORK_URL = 'https://api.instagram.com/v1/tags/newyork/media/recent?client_id=' + CLIENT_ID
+PARIS_URL = 'https://api.instagram.com/v1/tags/paris/media/recent?client_id=' + CLIENT_ID
